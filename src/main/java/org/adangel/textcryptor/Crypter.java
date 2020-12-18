66f8ff3a5a41b82b234ce4eb32e4527fffdf223f
Andreas Dangel <andreas.dangel@adangel.org>
2020-12-18 10:16:17 +0100

    Create random salt and iv

--- src/main/java/org/adangel/textcryptor/Crypter.java
+++ src/main/java/org/adangel/textcryptor/Crypter.java
@@ -6,6 +6,7 @@ import java.security.InvalidKeyException;
 import java.security.NoSuchAlgorithmException;
 import java.security.Provider;
 import java.security.Provider.Service;
+import java.security.SecureRandom;
 import java.security.Security;
 import java.security.spec.InvalidKeySpecException;
 
@@ -25,19 +26,17 @@ import javax.crypto.spec.SecretKeySpec;
 public class Crypter {
     private static final int iterationCount = 20;
     private static final int keyLength = 256; //AES_256
-    private final byte[] iv = new byte[] {1,2,3,4,5,6,7,8,9,10,11,12}; // 12 bytes
-    private final byte[] salt = new byte[] {1,2,3,4,5,6,7,8};
+    private static final int ivLength = 12; // 12 bytes
+    private static final int saltLength = 8; // 8 bytes
+    private SecureRandom random = new SecureRandom();
 
-    private Cipher getCipher(int mode, char[] pw) {
+    private Cipher getCipher(int mode, char[] pw, byte[] salt, byte[] iv) {
         try {
             SecretKeyFactory keyFactory = SecretKeyFactory.getInstance("PBKDF2WithHmacSHA512");
-            //byte[] salt = new byte[8];
-            //SecureRandom random = new SecureRandom();
-            //random.nextBytes(salt);
             PBEKeySpec keySpec = new PBEKeySpec(pw, salt, iterationCount, keyLength);
             SecretKey secretKey = keyFactory.generateSecret(keySpec);
             SecretKey secretKeyAes = new SecretKeySpec(secretKey.getEncoded(), "AES");
-            
+
             int myTLen = 128;
             GCMParameterSpec myParams = new GCMParameterSpec(myTLen, iv);
             Cipher cipher = Cipher.getInstance("AES_256/GCM/NoPadding");
@@ -60,22 +59,43 @@ public class Crypter {
             throw new RuntimeException(e);
         }
     }
-    
+
+    private byte[] createRandomSalt() {
+        byte[] bytes = new byte[saltLength];
+        random.nextBytes(bytes);
+        return bytes;
+    }
+
+    private byte[] createRandomIV() {
+        byte[] bytes = new byte[ivLength];
+        random.nextBytes(bytes);
+        return bytes;
+    }
+
     public byte[] encrypt(String text, char[] pw) {
         try {
-            Cipher cipher = getCipher(Cipher.ENCRYPT_MODE, pw);
-            
-            return cipher.doFinal(text.getBytes(StandardCharsets.UTF_8));
+            byte[] salt = createRandomSalt();
+            byte[] iv = createRandomIV();
+            Cipher cipher = getCipher(Cipher.ENCRYPT_MODE, pw, salt, iv);
+            byte[] data = cipher.doFinal(text.getBytes(StandardCharsets.UTF_8));
+            byte[] result = new byte[saltLength + ivLength + data.length];
+            System.arraycopy(salt, 0, result, 0, saltLength);
+            System.arraycopy(iv, 0, result, saltLength, ivLength);
+            System.arraycopy(data, 0, result, saltLength + ivLength, data.length);
+            return result;
         } catch (IllegalBlockSizeException | BadPaddingException e) {
             throw new RuntimeException(e);
         }
     }
-    
+
     public String decrypt(byte[] data, char[] pw) {
         try {
-            Cipher cipher = getCipher(Cipher.DECRYPT_MODE, pw);
-            
-            byte[] clear = cipher.doFinal(data);
+            byte[] salt = new byte[saltLength];
+            byte[] iv = new byte[ivLength];
+            System.arraycopy(data, 0, salt, 0, saltLength);
+            System.arraycopy(data, saltLength, iv, 0, ivLength);
+            Cipher cipher = getCipher(Cipher.DECRYPT_MODE, pw, salt, iv);
+            byte[] clear = cipher.doFinal(data, saltLength + ivLength, data.length - saltLength - ivLength);
             return new String(clear, StandardCharsets.UTF_8);
         } catch (IllegalBlockSizeException | BadPaddingException e) {
             //throw new RuntimeException(e);
--- src/main/java/org/adangel/textcryptor/actions/LoadAction.java
+++ src/main/java/org/adangel/textcryptor/actions/LoadAction.java
@@ -27,9 +27,11 @@ public class LoadAction extends AbstractAction {
         if (dialog.getEnteredPassword() != null) {
             System.out.println("after pw dialog: " + new String(dialog.getEnteredPassword()));
             byte[] data = StorageProvider.getSupported().load();
-            Crypter crypter = new Crypter();
-            String text = crypter.decrypt(data, dialog.getEnteredPassword());
-            textConsumer.accept(text);
+            if (data.length > 0) {
+                Crypter crypter = new Crypter();
+                String text = crypter.decrypt(data, dialog.getEnteredPassword());
+                textConsumer.accept(text);
+            }
         } else {
             JOptionPane.showMessageDialog(null, "No password given... exiting");
             System.exit(0);
