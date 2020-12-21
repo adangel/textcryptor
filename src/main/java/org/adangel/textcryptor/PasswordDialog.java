f70513945fe7ee9cf4a8d94304a832e3d5abc8e3
Andreas Dangel <andreas.dangel@adangel.org>
2020-12-21 14:45:48 +0100

    Apply Apache License 2.0

--- src/main/java/org/adangel/textcryptor/PasswordDialog.java
+++ src/main/java/org/adangel/textcryptor/PasswordDialog.java
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2020 Andreas Dangel
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package org.adangel.textcryptor;
 
 import java.awt.BorderLayout;
@@ -77,7 +93,8 @@ public class PasswordDialog extends JDialog {
             KeyAdapter keyListener = new KeyAdapter() {
                 @Override
                 public void keyTyped(KeyEvent e) {
-                    okButton.setEnabled(Arrays.equals(passwordField.getPassword(), repeat.getPassword()) && passwordField.getPassword().length > 0);
+                    okButton.setEnabled(Arrays.equals(passwordField.getPassword(), repeat.getPassword())
+                            && passwordField.getPassword().length > 0);
                 }
             };
             passwordField.addKeyListener(keyListener);
